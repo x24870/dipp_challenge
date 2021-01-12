7e885910b1a742b1e731d20af7a0b409f6b04454
pyk <x24870@gmail.com>
2021-01-12 11:29:59 +0800

    feat(all): retrieve image

--- app/controllers/image.py
+++ app/controllers/image.py
@@ -5,7 +5,7 @@ import os, re
 
 import requests
 from http import HTTPStatus
-from flask import Blueprint, jsonify, current_app, request
+from flask import Blueprint, jsonify, current_app, request, send_file
 
 from .resp import APIResponse
 from .image_helper import *
@@ -14,11 +14,12 @@ bp_image = Blueprint(name=__name__, import_name=__name__)
 
 @bp_image.route("/images/<filename>", methods=["GET"])
 def retrieve_image(filename):
-    """
-    Blueprint method to get one image from the server
-    """
-    return jsonify({}), HTTPStatus.OK
-
+    img_dir = current_app.config["IMAGES_DIR"]
+    if filename not in os.listdir(img_dir):
+        return APIResponse.not_found()
+    
+    abs_apth = os.path.join(img_dir, filename)
+    return send_file(abs_apth)
 
 @bp_image.route("/draw/", methods=["POST"])
 def draw_text_box():
@@ -46,4 +47,3 @@ def draw_text_box():
     draw_content(img_path, font_path, box, colors, json_resp)
     
     return APIResponse.success(json_resp)
-    
